1483e5f32e1ab3ca4d8ef0c7c3e9f2912a1ff538
vhochstein <v.hochstein@highstone.de>
2011-01-26 16:10:43 +0100

    Bugfix: autoloading for gem

--- lib/active_scaffold/helpers/view_helpers_override.rb
+++
@@ -1,8 +0,0 @@
-ActionView::Base.class_eval do
-  include ActiveScaffold::Helpers::UpdateColumnHelpers
-  if ActiveScaffold.js_framework == :jquery
-    include ActiveScaffold::Helpers::DatepickerUpdateColumnHelpers
-  elsif ActiveScaffold.js_framework == :prototype
-    include ActiveScaffold::Helpers::CalendarDateSelectUpdateColumnHelpers
-  end
-end
--- lib/active_scaffold_batch.rb
+++ lib/active_scaffold_batch.rb
@@ -3,9 +3,6 @@ ActiveScaffold rescue throw "should have included ActiveScaffold plug in first. 
 
 # Load our overrides
 require "#{File.dirname(__FILE__)}/active_scaffold_batch/config/core.rb"
-require "#{File.dirname(__FILE__)}/active_scaffold/config/batch_update.rb"
-require "#{File.dirname(__FILE__)}/active_scaffold/actions/batch_update.rb"
-require "#{File.dirname(__FILE__)}/active_scaffold/helpers/view_helpers_override.rb"
 
 module ActiveScaffoldBatch
   def self.root
@@ -13,12 +10,34 @@ module ActiveScaffoldBatch
   end
 end
 
+module ActiveScaffold
+  module Actions
+    ActiveScaffold.autoload_subdir('actions', self, File.dirname(__FILE__))
+  end
+
+  module Config
+    ActiveScaffold.autoload_subdir('config', self, File.dirname(__FILE__))
+  end
+
+  module Helpers
+    ActiveScaffold.autoload_subdir('helpers', self, File.dirname(__FILE__))
+  end
+end
+
+ActionView::Base.class_eval do
+  include ActiveScaffold::Helpers::UpdateColumnHelpers
+  if ActiveScaffold.js_framework == :jquery
+    include ActiveScaffold::Helpers::DatepickerUpdateColumnHelpers
+  elsif ActiveScaffold.js_framework == :prototype
+    include ActiveScaffold::Helpers::CalendarDateSelectUpdateColumnHelpers
+  end
+end
 
 ##
 ## Run the install assets script, too, just to make sure
 ## But at least rescue the action in production
 ##
-Rails::Application.initializer("active_scaffold_batch.install_assets") do
+Rails::Application.initializer("active_scaffold_batch.install_assets", :after => "active_scaffold.install_assets") do
   begin
     ActiveScaffoldAssets.copy_to_public(ActiveScaffoldBatch.root)
   rescue
