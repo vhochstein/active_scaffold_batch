b26f0d86ef6e843f8d0638d9293990f7e3d4874f
vhochstein <v.hochstein@highstone.de>
2011-11-23 16:47:43 +0100

    bump version to 3.1.0

--- lib/active_scaffold_batch/version.rb
+++ lib/active_scaffold_batch/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldBatch
   module Version
     MAJOR = 3
-    MINOR = 0
-    PATCH = 6
+    MINOR = 1
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
